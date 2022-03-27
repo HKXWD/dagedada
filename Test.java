97fc29cb70146efa705a44b9dbb489169240164c
hkx <1752348066@qq.com>
2022-03-27 17:30:02 +0800

    ddd

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test{
+public static void main(String[] args){
+	int count=0;
+	double paper=0.1;
+	int zf=8848430;
+	while(paper<=zf){paper*=2;count++;}
+	System.out.println("ĞèÒªµş£º"+count+"´Î");
+}}
